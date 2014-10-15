de82fe6de2810aa44724b339370af00a7a06a097
jianlee <1449190335@qq.com>
2014-10-15 22:43:04 +0800

    ASIRequestDemo
    ASIRequestDemo

--- RequestDemo0/ViewController.m
+++ RequestDemo0/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "ASIFormDataRequest.h"
+#import "JSONKit.h"
 @interface ViewController ()
 
 @end
@@ -17,7 +18,78 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initView];
 }
+
+-(void)initView
+{
+    
+    
+
+    
+    
+    UIButton*getBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    getBtn.frame = CGRectMake(50, 150, 60, 44);
+    [getBtn setTitle:@"GET" forState:UIControlStateNormal];
+    [getBtn addTarget:self action:@selector(getRequest) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:getBtn];
+    
+    
+    UIButton*postBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    postBtn.frame = CGRectMake(250, 150, 60, 44);
+    [postBtn setTitle:@"POST" forState:UIControlStateNormal];
+    [postBtn addTarget:self action:@selector(postRequst) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:postBtn];
+
+    
+    
+    
+    
+    
+}
+-(void)getRequest
+{
+    NSLog(@"GET");
+    NSString*str = @"http://api.map.baidu.com/telematics/v3/weather?location=北京&output=json&ak=5slgyqGDENN7Sy7pw29IUvrZ";
+    NSString*encodingStr =[str stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSURL*url = [NSURL URLWithString:encodingStr];
+     ASIHTTPRequest*request = [ASIHTTPRequest requestWithURL:url];
+    [request setCompletionBlock:^{
+        NSLog(@"123");
+        NSDictionary*dic= [request.responseData objectFromJSONData];
+        NSLog(@"---GET Response Dic---:%@",dic);
+
+    }];
+    [request startAsynchronous];
+    
+}
+
+
+
+
+
+-(void)postRequst
+{
+    NSLog(@"POST");
+    NSURL*url = [NSURL URLWithString:@""];
+  __block  ASIFormDataRequest*request = [ASIFormDataRequest requestWithURL:url];
+    
+//    [request setValue:<#(id)#> forKey:<#(NSString *)#>];
+//    [request setValue:<#(id)#> forKey:<#(NSString *)#>];
+    
+    [request setCompletionBlock:^{
+        NSDictionary*dic = [request.responseData objectFromJSONData];
+        NSLog(@"---POST Response Dic---:%@",dic);
+    }];
+    [request startAsynchronous];
+}
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
